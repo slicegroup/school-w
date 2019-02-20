6efe44a8aa57e77f5b1c774d7072f3814d9c0068
patricia <yively.pa@gmail.com>
2019-02-20 15:51:21 -0400

    modal

--- wp-content/themes/Student/partials/contact.php
+++ wp-content/themes/Student/partials/contact.php
@@ -5,7 +5,7 @@
       <div class="center">
         <h1 class="animated wow fadeInUp" data-wow-duration="1s" data-wow-delay=".2s">INTERESTED?
 PLEASE CONTACT US</h1>
-        <a class="slide button">contact</a>
+<a class="slide button"  data-toggle="modal" data-target="#exampleModalCenter"> contact</a>
       </div>
     </div>
   </div>
