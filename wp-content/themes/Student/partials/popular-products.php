a2979ef2d32b91bc3c9bdfdca6348f08928b2ec0
patricia <yively.pa@gmail.com>
2019-02-22 08:52:56 -0400

    imagen product

--- wp-content/themes/Student/partials/popular-products.php
+++ wp-content/themes/Student/partials/popular-products.php
@@ -11,9 +11,7 @@
             <a href="<?php the_permalink() ?>">
               <div class="
               img-product">
-              <?php if ( get_field( 'image') ) { ?>
-                <img src="<?php the_field( 'image' ); ?>" />
-              <?php } ?>
+              <?php the_post_thumbnail('medium'); ?>
             </div>
             <div class="text-product">
               <span><?php the_title(); ?></span>
